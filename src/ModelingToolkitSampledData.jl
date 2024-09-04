915a5417fdf6a95b5cb814d699b0f1414267571a
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2024-09-04 07:30:44 +0200

    add exponential filter component

--- src/ModelingToolkitSampledData.jl
+++ src/ModelingToolkitSampledData.jl
@@ -7,7 +7,8 @@ export get_clock
 export DiscreteIntegrator, DiscreteDerivative, Delay, Difference, ZeroOrderHold, Sampler,
        ClockChanger,
        DiscretePIDParallel, DiscretePIDStandard, DiscreteStateSpace,
-       DiscreteTransferFunction, NormalNoise, UniformNoise, Quantization
+       DiscreteTransferFunction, NormalNoise, UniformNoise, Quantization,
+       ExponentialFilter
 include("discrete_blocks.jl")
 
 end
